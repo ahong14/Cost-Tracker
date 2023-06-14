e611565436f7f05b7c51c59c34f135e00603349b
Alex Hong <alexhong681@gmail.com>
2023-06-13 18:03:54 -0700

    basic unit/integration tests

--- spring/cost_tracker/src/test/java/com/cost_tracker/cost_tracker/UserServiceImplIntegrationTest.java
+++ spring/cost_tracker/src/test/java/com/cost_tracker/cost_tracker/UserServiceImplIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,8 +22,7 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class UserServiceImplIntegrationTest {
@@ -49,11 +49,12 @@ public class UserServiceImplIntegrationTest {
     public void setUp() {
         User testUser = new User(1, "Test", "Test Lastname", "test@mail.com", "test password");
         User testUser2 = new User(2, "Test", "Test Lastname", "test2@mail.com", "test password");
+        User testUser3 = new User(2, "Test", "Test Lastname", "test3@mail.com", "test password");
 
-        UserRepository mockUserRepository = mock(UserRepository.class);
-        when(mockUserRepository.findUserByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
-        when(mockUserRepository.findUserByEmail(testUser2.getEmail())).thenReturn(Optional.ofNullable(null));
-        when(mockUserRepository.save(any(User.class))).thenReturn(new User());
+
+        when(userRepository.findUserByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        when(userRepository.findUserByEmail(testUser2.getEmail())).thenReturn(Optional.ofNullable(null));
+        when(userRepository.save(any(User.class))).thenReturn(new User());
     }
 
 
@@ -65,10 +66,18 @@ public class UserServiceImplIntegrationTest {
         userService.createUser(testUser);
     }
 
-//    @Test
-//    public void creatingUserNotFoundEmailCreated() {
-//        User testUser2 = new User(2, "Test", "Test Lastname", "test2@mail.com", "test password");
-//        User createdUser = userService.createUser(testUser2);
-//        assert (createdUser != null);
-//    }
+    @Test
+    public void creatingUserNotFoundEmailCreated() {
+        User testUser2 = new User(2, "Test", "Test Lastname", "test2@mail.com", "test password");
+        User createdUser = userService.createUser(testUser2);
+        assert (createdUser != null);
+    }
+
+    @Test
+    public void loginUserNotFoundExceptionThrown() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("User email not found");
+        User testUser2 = new User(2, "Test", "Test Lastname", "test2@mail.com", "test password");
+        userService.loginUser(testUser2.getEmail(), testUser2.getPassword());
+    }
 }
